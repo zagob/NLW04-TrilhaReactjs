20393eebdd2fedebefb7f21b77ec5922765c0f85
zagob <matheusus1_23@hotmail.com>
2021-02-28 01:18:25 -0300

    Moveit

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,57 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
+import styles from '../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+    const { resetCountdown } = useContext(CountdownContext);
+    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+
+    function handleChallengeSuccess() {
+        completeChallenge();
+        resetCountdown();
+    }
+
+    function handleChallengeFail() {
+        resetChallenge();
+        resetCountdown();
+    }
+
+    return (
+        <div className={styles.challengeBoxContainer}>
+            { activeChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe {activeChallenge.amount} xp</header>
+
+                    <main>
+                        <img src={`icons/${activeChallenge.type}.svg`} />
+                        <strong>Novo desafio</strong>
+                        <p>{activeChallenge.description}</p>
+                    </main>
+
+                    <footer>
+                        <button
+                            type="button"
+                            className={styles.challengeFailedButton}
+                            onClick={handleChallengeFail}
+                        >Falhei</button>
+
+                        <button
+                            type="button"
+                            className={styles.challengeSuccessButton}
+                            onClick={handleChallengeSuccess}
+                        >Completei</button>
+                    </footer>
+                </div>
+            ) : (
+                    <div className={styles.challengeNotActive}>
+                        <strong>Finalize um ciclo para receber um desafio</strong>
+                        <p>
+                            <img src="icons/level-up.svg" alt="Level up" />
+                    Avance de level completando desafios
+                </p>
+                    </div>
+                )}
+        </div>
+    )
+}
